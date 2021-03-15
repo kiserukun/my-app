a952802f13206b2ef6ea4387905be8de491eae95
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-03-15 16:36:31 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './components/Navigation';
+import Field from './components/Field';
+import Button from './components/Button';
+import ManipulationPanel from './components/ManipulationPanel';
+import {initFields } from'./utils'
+
+const fields = initFields(35)
+
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <div className="title-container">
-          <h1 className="title>Snake Game"></h1>
+          <h1 className="title">Snake Game</h1>
         </div>
         <Navigation />
       </header>
       <main className="main">
-        <Field />
+        <Field fields={fields} />
       </main>
       <footer className="footer">
         <Button />
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation = () =>{
+  return<p>nabvivi</p>
+};
+
+export default Navigation;
